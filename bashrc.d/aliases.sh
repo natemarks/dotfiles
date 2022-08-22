134820cedee587c0bc2686c0834105a0ffa35835
Nate Marks <npmarks@gmail.com>
2022-08-22 06:20:54 -0400

    remove ~/go/src from fzf find aliases

--- bashrc.d/aliases.sh
+++ bashrc.d/aliases.sh
@@ -8,7 +8,10 @@ alias do_prune_everything='docker system prune -a'
 # alias fzgef='gedit $(find . -type f 2>/dev/null  | fzf)'
 
 alias cdh='cd $(find ~ -maxdepth 1 -type d | fzf)'
-alias cdp='cd $(find ~/projects ~/go/src -maxdepth 2 -type d | fzf)'
+alias cdp='cd $(find ~/projects -maxdepth 2 -type d | fzf)'
+# ~/go/... is a part of the deprecated GOPATH config superceded by go modules
+# https://insujang.github.io/2020-04-04/go-modules/
+# alias cdp='cd $(find ~/projects ~/go/src -maxdepth 2 -type d | fzf)'
 alias vim='nvim'
 #alias aws_imprivata-sandbox='eval "$(gpg --decrypt --recipient npmarks@gmail.com ~/bin/aws_imprivata-sandbox.sh.gpg)"'
 alias aws_i-platform-dev='eval "$(gpg --decrypt --recipient npmarks@gmail.com ~/bin/aws_i-platform-dev.sh.gpg)"'
@@ -17,7 +20,7 @@ alias aws_imprivata-natemarks='eval "$(gpg --decrypt --recipient npmarks@gmail.c
 # use vscode in current directory
 alias vcc='code -n .'
 # open project in vscode using fzf
-alias vcp='code -n $(find ~/projects ~/go/src -maxdepth 2 -type d | fzf)'
+alias vcp='code -n $(find ~/projects -maxdepth 2 -type d | fzf)'
 # aws account aliases
 alias aws_imprivata-sandbox='export AWS_PROFILE=imprivata-sandbox'
 alias aws_platform-dev='export AWS_PROFILE=platform-dev'
