ed2fe1824ad3eabcccba6e54d16c3be96a7de057
Nate Marks <npmarks@gmail.com>
2022-08-27 05:30:38 -0400

    install  gopls

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -9,3 +9,5 @@ curl -L  "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz" \
 
 tar -xzvf ~/bin/go/${GO_VERSION}/go${GO_VERSION}.linux-amd64.tar.gz \
 --directory ~/bin/go/"${GO_VERSION}"
+
+go install -v golang.org/x/tools/gopls@latest
