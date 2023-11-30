28b1fa4c23c1a7fe73c0c98dba906583cd18da7d
Nate Marks <npmarks@gmail.com>
2023-11-30 05:15:13 -0500

    add bash function ec2_name_from_id

--- bashrc.d/aws_functions.sh
+++ bashrc.d/aws_functions.sh
@@ -3,3 +3,7 @@
 ssmconnect() {
   aws ssm start-session --target "${1}"
 }
+ec2_name_from_id() {
+  # shellcheck disable=2016
+  aws ec2 describe-instances --instance-ids "$1" --query 'Reservations[*].Instances[*].Tags[?Key==`Name`].Value' --output text
+}
