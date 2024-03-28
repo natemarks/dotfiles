904b15f887732a68e4826e15d597fa5a4d683653
Nate Marks <npmarks@gmail.com>
2024-03-28 09:43:01 -0400

    neotree

--- neovim/lua/pluginlist.lua
+++ neovim/lua/pluginlist.lua
@@ -87,6 +87,16 @@ return {
                 theme = 'onedark',
             })
         end,
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
     }
 
 }
