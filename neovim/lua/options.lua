6131f13b394df1c10d30a2c24002fac7bb52e5d4
Nate Marks <npmarks@gmail.com>
2024-10-28 08:28:30 -0400

    sdf telescoper buffers hotley

--- neovim/lua/options.lua
+++ neovim/lua/options.lua
@@ -106,6 +106,7 @@ vim.opt.writebackup = false
 -- telescope settings
 vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>fg', ':Telescope live_grep<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope buffers<CR>', {noremap = true})
 
 -- neo-tree.nvim settings
 -- open file system
