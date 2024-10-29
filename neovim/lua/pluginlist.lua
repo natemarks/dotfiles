710275713e0917b493086d1371407c2d20c31c7d
Nate Marks <npmarks@gmail.com>
2024-10-29 12:21:21 -0400

    add harpoon

--- neovim/lua/pluginlist.lua
+++ neovim/lua/pluginlist.lua
@@ -41,7 +41,7 @@ return {
         build = ':TSUpdate',
 
         config = function()
-            require'nvim-treesitter.configs'.setup {
+            require 'nvim-treesitter.configs'.setup {
                 ensure_installed = {
                     'go',
                     'gomod',
@@ -99,6 +99,43 @@ return {
             "MunifTanjim/nui.nvim",
             -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         }
-    }
+    },
+    {
+        "ThePrimeagen/harpoon",
+        lazy = false,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            require("harpoon").setup({})
 
+            local function toggle_telescope_with_harpoon(harpoon_files)
+                local file_paths = {}
+                for _, item in ipairs(harpoon_files.items) do
+                    table.insert(file_paths, item.value)
+                end
+
+                require("telescope.pickers")
+                        .new({}, {
+                    prompt_title = "Harpoon",
+                    finder = require("telescope.finders").new_table({
+                        results = file_paths,
+                    }),
+                    previewer = require("telescope.config").values.file_previewer({}),
+                    sorter = require("telescope.config").values.generic_sorter({}),
+                })
+                        :find()
+            end
+            vim.keymap.set("n", "<leader>a", function()
+                local harpoon = require("harpoon")
+                toggle_telescope_with_harpoon(harpoon:list())
+            end, { desc = "Open harpoon window" })
+        end,
+        keys = {
+            { "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
+            { "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
+            { "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
+            { "<leader>ha", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+        },
+    }
 }
