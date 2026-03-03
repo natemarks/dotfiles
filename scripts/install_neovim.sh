427858e4a9a66df4add2f089deacd5db846be5f4
Nate Marks <npmarks@gmail.com>
2026-03-03 05:21:44 -0500

    sdf

--- scripts/install_neovim.sh
+++ scripts/install_neovim.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-NVIM_VERSION=0.10.2
-NVIM_TARBALL="nvim-linux64.tar.gz"
+NVIM_VERSION="0.11.6"
+NVIM_SHA256SUM="2fc90b962327f73a78afbfb8203fd19db8db9cdf4ee5e2bef84704339add89cc"
+NVIM_TARBALL="nvim-linux-x86_64.tar.gz"
+NVIM_INSTALL_DIR="/opt/neovim/${NVIM_VERSION}"
+NVIM_BIN="/opt/neovim/${NVIM_VERSION}/nvim-linux-x86_64/bin/nvim"
+NVIM_LINK="${HOME}/bin/nv"
 
-# https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
 TARBALL_URL="https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/${NVIM_TARBALL}"
 SHA256SUM_URL="${TARBALL_URL}.sha256sum"
 
-curl -JLO "${SHA256SUM_URL}"
-curl -JLO "${TARBALL_URL}"
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
-if sha256sum -c "${NVIM_TARBALL}.sha256sum"; then
-  tar xzvf "${NVIM_TARBALL}"
-  sudo rm -rf /opt/nvim
-  sudo tar -C /opt -xzf "${NVIM_TARBALL}"
-  sudo rm -f /usr/local/bin/vim
-  sudo ln -s /usr/local/bin/nvim /usr/local/bin/vim
-  rm -rf nvim-linux64 "${NVIM_TARBALL}" "${NVIM_TARBALL}.sha256sum"
-else
-  echo "Checksum failed"
-  exit 1
+source "$SCRIPT_DIR/helper.sh"
+
+if [ -d "${NVIM_INSTALL_DIR}" ]; then
+  echo "install dir exists: ${NVIM_INSTALL_DIR}. exiting"
+  exit
 fi
+
+curl -JLO "${TARBALL_URL}"
+check_sha256 "${NVIM_TARBALL}" "${NVIM_SHA256SUM}"
+sudo mkdir -p "$NVIM_INSTALL_DIR"
+sudo tar -C "${NVIM_INSTALL_DIR}" -xzf "${NVIM_TARBALL}"
+rm -f "${NVIM_LINK}"
+ln -s "${NVIM_BIN}" "${NVIM_LINK}"
+ls -la "${NVIM_LINK}"
+rm -rf nvim-linux64 "${NVIM_TARBALL}" "${NVIM_TARBALL}.sha256sum"
