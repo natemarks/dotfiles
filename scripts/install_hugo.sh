e5ec84d94587db767f00848549ef8fcb6d90b5df
Nate Marks <npmarks@gmail.com>
2026-03-11 13:13:21 -0400

    hugo extended

--- scripts/install_hugo.sh
+++ scripts/install_hugo.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 # https://github.com/gohugoio/hugo/releases/download/v0.157.0/hugo_0.157.0_linux-amd64.tar.gz
 HUGO_VERSION="0.157.0"
-TARBALL="hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
+TARBALL="hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
 URL="https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${TARBALL}"
 echo "URL: ${URL}"
 curl -JLO "${URL}"
