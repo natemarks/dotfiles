475a63e101a766ba0b300c1a20056580806d133c
Nate Marks <npmarks@gmail.com>
2026-03-11 08:53:37 -0400

    hugo

---
+++ scripts/install_hugo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+# https://github.com/gohugoio/hugo/releases/download/v0.157.0/hugo_0.157.0_linux-amd64.tar.gz
+HUGO_VERSION="0.157.0"
+TARBALL="hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
+URL="https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${TARBALL}"
+echo "URL: ${URL}"
+curl -JLO "${URL}"
+tar -xvf "${TARBALL}" -C "${HOME}/bin" hugo
+rm -r "${TARBALL}"
