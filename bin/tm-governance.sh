82b2881a497f13e9540235699c53c3b976be7473
Nate Marks <npmarks@gmail.com>
2026-04-04 07:12:54 -0400

    tm-governance.sh fix n Please enter the commit message for your changes. Lines starting

--- bin/tm-governance.sh
+++ bin/tm-governance.sh
@@ -11,8 +11,7 @@ SESSION_NAME="opencode"
 # Check if the tmux session exists
 if ! tmux has-session -t $SESSION_NAME >/dev/null 2>&1; then
   # If session doesn't exist, create a new one
-  tmux new-session -d -s $SESSION_NAME
-  tmux new-window -t $SESSION_NAME -n gov-oc -c ~/projects/aws-governance/
+  tmux new-session -d -s $SESSION_NAME -n gov-oc -c ~/projects/aws-governance/
   tmux new-window -t $SESSION_NAME -n gov-nv -c ~/projects/aws-governance/ "nv"
   tmux new-window -t $SESSION_NAME -n nplza-oc -c ~/projects/non-prod-aws-accelerator-config/
   tmux new-window -t $SESSION_NAME -n nplza-nv -c ~/projects/non-prod-aws-accelerator-config/ "nv"
