2d898e0e3687257f52f51d1063adc051832d2f47
Nate Marks <npmarks@gmail.com>
2026-05-20 08:04:07 -0400

    update safe pull

--- bin/safe_git_pull.sh
+++ bin/safe_git_pull.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 if git diff --quiet && git diff --cached --quiet; then
+  current_branch=$(git branch --show-current)
+  if [ "$current_branch" = "main" ]; then
+    git branch --set-upstream-to=origin/main main
+  fi
   git pull
 else
   echo "Repo has local changes; skipping pull"
